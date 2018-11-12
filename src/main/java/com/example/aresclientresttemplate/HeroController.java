5ab3ec6d84b23dd3c3b155112d91f111cb1af877
Joe Hong <jhng323@gmail.com>
2018-11-11 19:05:19 -0500

    add return of single object by ID

--- src/main/java/com/example/aresclientresttemplate/HeroController.java
+++ src/main/java/com/example/aresclientresttemplate/HeroController.java
@@ -3,36 +3,66 @@ package com.example.aresclientresttemplate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @Slf4j
-@RequestMapping("/hero")
+@RequestMapping("/heroes")
 public class HeroController {
 
     @Autowired
     private RestTemplate restTemplate;
 
-    private static final String URL = "http://localhost:8080/heroes";
+    private static final String BASE_URL = "http://localhost:8080";
 
-    @RequestMapping(value = "/all", method = RequestMethod.GET)
-    public Collection<Hero> getHero() {
-        ResponseEntity<Resources<Hero>> heroResponse = restTemplate
-                .exchange(URL, HttpMethod.GET, null,
-                        new ParameterizedTypeReference<Resources<Hero>>() {
-                        });
-        return heroResponse.getBody().getContent();
+    // return all objects
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Collection<Hero> getHeroes() {
+        String url = BASE_URL + "/heroes";
+        ResponseEntity<Resources<Hero>> responseEntity = restTemplate
+                .exchange(url, HttpMethod.GET, null,
+                        new ParameterizedTypeReference<Resources<Hero>>() {});
+        return responseEntity.getBody().getContent();
     }
 
-    // TODO: CRUD operations
+/*    @RequestMapping(value = "/{firstName}", method = RequestMethod.GET)
+    public Collection<Hero> getHeroByFirstName(@PathVariable("firstName") String firstName) {
+        String url = BASE_URL + "/heroes/search/findByFirstName?firstname=" + firstName;
+        ResponseEntity<Resources<Hero>> responseEntity = restTemplate.exchange(url, HttpMethod.GET,
+                null, new ParameterizedTypeReference<Resources<Hero>>() {});
+        return responseEntity.getBody().getContent();
+    }*/
+
+    // Return single object by ID
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public Hero getHeroById(@PathVariable("id") Long id) {
+        String url = BASE_URL + "/heroes/" + id;
+        ResponseEntity<Resource<Hero>> responseEntity = restTemplate.exchange(url, HttpMethod.GET,
+                null, new ParameterizedTypeReference<Resource<Hero>>() {},
+                Collections.emptyMap());
+        return responseEntity.getBody().getContent();
+    }
+
+    // TODO: POST single object
+    // TODO: POST list of objects
+    // TODO: PUT update single object
+    // TODO: PUT update list of object
+    // TODO: DELETE single object
+    // TODO: DELETE list of objects
+    // TODO: DELETE all objects
 
     // TODO: UI javascript to present data
 }
